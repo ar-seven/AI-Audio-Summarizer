1efa719771ad58c6671f5babfc76702ca466b162
Aravind <raravind600@gmail.com>
2024-01-15 22:20:35 +0530

    fixed repo

--- transcriber.py
+++
@@ -1,9 +0,0 @@
-import assemblyai as aai
-
-aai.settings.api_key = "3852cdfd5575446487db8b9b621dbd5a"
-file = '' #the input file from the user
-transcriber = aai.Transcriber()
-transcript = transcriber.transcribe(file)
-
-# print(transcript.text)
-txt = transcript.text  #transcribed text
